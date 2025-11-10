9ad157d38ba0ab012f6e09efdb55fc9f19da561b
Morgan Johansson <morgan.johansson@me.com>
2025-11-10 13:25:53 +0100

    Add fully responsive Question Form Section for service detail pages

---
+++ Onatrix/ViewModels/QuestionFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Onatrix.ViewModels;
+
+public class QuestionFormViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email")]
+    [RegularExpression(@"^(([^<>()[\]\\.,;:\s@\""]+(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", ErrorMessage = "Invalid email address")]
+    public string QuestionEmail { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please enter your question")]
+    [Display(Name = "Question")]
+    public string Question { get; set; } = null!;
+}
